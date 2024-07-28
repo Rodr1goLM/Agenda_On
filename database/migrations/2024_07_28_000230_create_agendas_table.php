c60492eea5b5683ccf92ed1d4d3d3a2c4d09a189
rodr1golm <lmlindnermoraes@gmail.com>
2024-07-27 23:09:12 -0300

    Inclusão de migrations e classes

---
+++ database/migrations/2024_07_28_000230_create_agendas_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agenda', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('medico')->constrained('medicos');
+            $table->foreignId('unidade')->constrained('unidades');
+            $table->date('dia');
+            $table->boolean('h_00')->default(false);
+            $table->boolean('h_01')->default(false);
+            $table->boolean('h_02')->default(false);
+            $table->boolean('h_03')->default(false);
+            $table->boolean('h_04')->default(false);
+            $table->boolean('h_05')->default(false);
+            $table->boolean('h_06')->default(false);
+            $table->boolean('h_07')->default(false);
+            $table->boolean('h_08')->default(false);
+            $table->boolean('h_09')->default(false);
+            $table->boolean('h_10')->default(false);
+            $table->boolean('h_11')->default(false);
+            $table->boolean('h_12')->default(false);
+            $table->boolean('h_13')->default(false);
+            $table->boolean('h_14')->default(false);
+            $table->boolean('h_15')->default(false);
+            $table->boolean('h_16')->default(false);
+            $table->boolean('h_17')->default(false);
+            $table->boolean('h_18')->default(false);
+            $table->boolean('h_19')->default(false);
+            $table->boolean('h_20')->default(false);
+            $table->boolean('h_21')->default(false);
+            $table->boolean('h_22')->default(false);
+            $table->boolean('h_23')->default(false);
+            $table->timestamps();
+
+            $table->unique(['medico', 'dia']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agendas');
+    }
+};
---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.template')
+
+@section('content')
+    <h1 class="text-center">Bem-vindo à Página Inicial</h1>
+    <p>Este é o conteúdo da página inicial utilizando template padrão com a diretiva yield.</p>
+@endsection
